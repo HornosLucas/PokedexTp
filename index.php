d2b9c4afb404ab753a8144f0bb303fb228ef0fa1
HornosLucas <lhornos705@alumno.unlam.edu.ar>
2024-09-16 11:52:52 -0300

    modal modificacion pokemon

--- index.php
+++ index.php
@@ -42,10 +42,45 @@
         </td>
         <td> <p class="info"> #123 </p> </td>
         <td> <p class="info"> Scyther </p> </td>
-        <td> <button class="w3-button w3-black  w3-hover-red w3-round-large">eliminar</button> <button class="w3-button w3-black  w3-round-large">modificar</button></td>
+        <td>
+            <button class="w3-button w3-black  w3-hover-red w3-round-large">eliminar</button>
+            <button onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-black  w3-round-large">modificar</button>
+        </td>
     </tr>
 
 </table>
+
+    <div id="id01" class="w3-modal">
+        <div class="w3-modal-content w3-animate-top">
+
+            <header class="w3-container w3-teal">
+            <span onclick="document.getElementById('id01').style.display='none'"
+            class="w3-button w3-display-topright">&times;</span>
+                <h2>Modificar pokemon</h2>
+            </header>
+
+            <div class="">
+                <form class="w3-container w3-card-4" action=""> <!--- MODIFICAR ACTION Y PONER PHP --->
+                    <br>
+                    <p>
+                        <label class="w3-text-grey">Nombre</label>
+                        <input class="w3-input w3-border" type="text" required="">
+                    </p>
+                    <p>
+                        <label class="w3-text-grey">Numero</label>
+                        <input class="w3-input w3-border" type="text" required="">
+                    </p>
+                    <p>
+                        <label class="w3-text-grey">Descripcion</label>
+                        <textarea class="w3-input w3-border" style="resize:none"></textarea>
+                    </p>
+
+                    <p><button type="button" class="w3-btn w3-padding w3-teal w3-right w3-margin-bottom" style="width:120px">Modificar</button></p>
+                </form>
+
+            </div>
+        </div>
+    </div>
 </main>
 
 <footer>
