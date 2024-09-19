c0ff6dc7fe459a6f9b4a210ebc33986ae42a62e0
IgnacioMoyano <110944396+IgnacioMoyano@users.noreply.github.com>
2024-09-19 16:12:31 -0300

    Crear_pokemon

--- index.php
+++ index.php
@@ -126,6 +126,79 @@
             </div>
         </div>
 
+        <div class="newPokemon">
+            <button onclick=document.getElementById('id03').style.display='block' class="w3-button w3-black w3-hover-blue  w3-round-large w3-right w3-margin-bottom"> Capturar Nuevo Pokemon</button>
+        </div>
+
+        <div id="id03" class="w3-modal">
+            <div class="w3-modal-content w3-animate-top">
+
+                <header class="w3-container w3-teal">
+            <span onclick="document.getElementById('id03').style.display='none'"
+                  class="w3-button w3-hover-red w3-display-topright">&times;</span>
+                    <h2>Capturar pokemon</h2>
+                </header>
+
+                <div class="">
+                    <form class="w3-container w3-card-4" enctype="multipart/form-data" action="src/crearPokemon.php" method="post"> <!--- MODIFICAR ACTION Y PONER PHP --->
+                        <br>
+                        <p>
+                            <label class="w3-text-grey">Imagen</label>
+                            <input class="w3-input w3-border" type="file" required="" name="imagen">
+                        </p>
+                        <p>
+                            <label class="w3-text-grey">Nombre</label>
+                            <input class="w3-input w3-border" type="text" required="" name="nombre">
+                        </p>
+                        <p>
+                            <label class="w3-text-grey">Tipo</label>
+
+                                <?php
+                                $tipos = parse_ini_file("img/tipos/tipo.ini", true);
+
+                                echo "<select name='tipo' class='w3-input w3-border'>";
+
+                                foreach ($tipos['tipos'] as $tipo) {
+                                    echo '<option value="' . $tipo . '">' . $tipo . '</option>';
+                                }
+                                echo '</select>';
+                                ?>
+
+
+                        </p>
+                        <p>
+                            <label class="w3-text-grey">Segundo Tipo</label>
+                            <?php
+                            $tipos = parse_ini_file("img/tipos/tipo.ini", true);
+
+                            echo "<select name='segundoTipo' class='w3-input w3-border'>";
+                            echo "<option value=''>Ninguno</option>";
+                            foreach ($tipos['tipos'] as $tipo) {
+                                echo '<option value="' . $tipo . '">' . $tipo . '</option>';
+                            }
+                            echo '</select>';
+                            ?>
+                        </p>
+                        <p>
+                            <label class="w3-text-grey">Numero</label>
+                            <input class="w3-input w3-border" type="number" required="" name="numero">
+                        </p>
+                        <p>
+                            <label class="w3-text-grey">Descripcion</label>
+                            <textarea class="w3-input w3-border" style="resize:none" name="descripcion"></textarea>
+                        </p>
+
+                        <p>
+                            <button type="submit"
+                                    class="w3-button w3-black w3-hover-blue  w3-round-large w3-right w3-margin-bottom"
+                                    style="width:120px">Capturar
+                            </button>
+                        </p>
+                    </form>
+
+                </div>
+            </div>
+        </div>
 
     </main>
 
--- src/crearPokemon.php
+++ src/crearPokemon.php
@@ -1,1 +1,38 @@
 <?php
+
+include_once('db.php');
+
+$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+$numero = isset($_POST['numero']) ? $_POST['numero'] : '';
+$tipo = isset($_POST['tipo']) ? $_POST['tipo'] : '';
+$segundoTipo = isset($_POST['segundoTipo']) ? $_POST['segundoTipo'] : '';
+$descripcion = isset($_POST['descripcion']) ? $_POST['descripcion'] : '';
+
+$carpeta_destino =  "../img/pokemon/";
+
+if (isset($_FILES['imagen']) && $_FILES['imagen']['error'] == 0) {
+
+    $imagen_urlTemp = $_FILES['imagen']['tmp_name'];
+    $imagen_nombre = $_FILES['imagen']['name'];
+
+
+
+    if (move_uploaded_file($imagen_urlTemp, $carpeta_destino . $imagen_nombre)) {
+
+        $queryCrearPokemon = "INSERT INTO pokemon (id_pokemon, imagen, nombre, tipo, tipo_2, descripcion) VALUES ('$numero', 'img/pokemon/" . $imagen_nombre . "', '$nombre', '$tipo', '$segundoTipo', '$descripcion')";
+
+        if ($conn->query($queryCrearPokemon) === TRUE) {
+            header("Location: ../index.php");
+            exit();
+        } else {
+            echo "Error en la consulta SQL: " . $conn->error;
+        }
+    } else {
+        echo "Error al subir el archivo";
+    }
+} else {
+    echo "No se seleccionó ningún archivo o hubo un error al subirlo.";
+}
+?>
+
+
