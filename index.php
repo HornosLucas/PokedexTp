a278de777e9dc3e7405ac9fe3e26bc9d1b981212
Santy <pizarro.santy@gmail.com>
2024-09-20 22:06:48 -0300

    modificarPokemon.php
    falta una validacion para los tipos y css

--- index.php
+++ index.php
@@ -115,16 +115,16 @@
                         <p>
                             <label class="w3-text-grey">Tipo</label>
 
-                                <?php
-                                $tipos = parse_ini_file("img/tipos/tipo.ini", true);
+                            <?php
+                            $tipos = parse_ini_file("img/tipos/tipo.ini", true);
 
-                                echo "<select name='tipo' class='w3-input w3-border'>";
+                            echo "<select name='tipo' class='w3-input w3-border'>";
 
-                                foreach ($tipos['tipos'] as $tipo) {
-                                    echo '<option value="' . $tipo . '">' . $tipo . '</option>';
-                                }
-                                echo '</select>';
-                                ?>
+                            foreach ($tipos['tipos'] as $tipo) {
+                                echo '<option value="' . $tipo . '">' . $tipo . '</option>';
+                            }
+                            echo '</select>';
+                            ?>
 
 
                         </p>
@@ -196,9 +196,9 @@ function traerTodosLosPokemons($resultado, string $path_tipos)
         while ($fila = $resultado->fetch_assoc()) {
             echo "<tr>";
             echo "<td><img src='" . $fila['imagen'] . "'  alt='Imagen de Pokemon'></td>";
-            echo "<td><img src='" . $path_tipos . $fila['tipo'] . ".png" . "' class='w3-circle' alt='Tipo de Pokemon'>";
+            echo "<td><img src='" . $path_tipos . $fila['tipo'] . ".png' class='w3-circle' alt='Tipo de Pokemon'>";
             if (!empty($fila["tipo_2"])) {
-                echo "<img src='" . $path_tipos . $fila['tipo_2'] . ".png" . "' class='w3-circle' alt='Tipo Secundario de Pokemon'></td>";
+                echo "<img src='" . $path_tipos . $fila['tipo_2'] . ".png' class='w3-circle' alt='Tipo Secundario de Pokemon'></td>";
             } else {
                 echo "</td>";
             }
@@ -209,29 +209,45 @@ function traerTodosLosPokemons($resultado, string $path_tipos)
 
             echo "<button type='button' class='w3-button w3-black w3-hover-red w3-round-large' onclick=\"confirmDelete('" . $fila['id_pokemon'] . "')\">Eliminar</button>";
 
-
-            echo "<button type='button' onclick=\"document.getElementById('id01').style.display='block'\" class='w3-button w3-black w3-hover-blue w3-round-large'>Modificar</button>";
+            echo "<button type='button' onclick=\"document.getElementById('modal-modificar-" . $fila['id_pokemon'] . "').style.display='block'\" class='w3-button w3-black w3-hover-blue w3-round-large'>Modificar</button>";
 
             echo "</td>";
             echo "</tr>";
 
-
-            echo "<div id='modal-" . $fila['id_pokemon'] . "' class='w3-modal'>
-                    <div class='w3-modal-content w3-animate-top'>
-                        <header class='w3-container w3-teal'>
-                            <span onclick=\"document.getElementById('modal-" . $fila['id_pokemon'] . "').style.display='none'\"
-                                   class='w3-button w3-hover-red w3-display-topright'>&times;</span>
-                            <h2>Eliminar Pokémon</h2>
-                        </header>
-                        <div class='w3-container'>
-                            <p>¿Estás seguro de que deseas eliminar a " . $fila['nombre'] . "?</p>
-                        </div>
-                        <footer class='w3-container w3-red'>
-                            <button id='btnConfirmDelete-" . $fila['id_pokemon'] . "' class='w3-button w3-green w3-right'>Sí</button>
-                            <button onclick=\"document.getElementById('modal-" . $fila['id_pokemon'] . "').style.display='none'\" class='w3-button w3-red w3-right'>No</button>
-                        </footer>
+            echo "
+            <div id='modal-modificar-" . $fila['id_pokemon'] . "' class='w3-modal'>
+                <div class='w3-modal-content w3-animate-top'>
+                    <header class='w3-container w3-teal'>
+                        <span onclick=\"document.getElementById('modal-modificar-" . $fila['id_pokemon'] . "').style.display='none'\"
+                            class='w3-button w3-hover-red w3-display-topright'>&times;</span>
+                        <h2>Modificar Pokémon</h2>
+                    </header>
+                    <div class='w3-container'>
+                        <form action='/pokedexTp/src/modificarPokemon.php' method='POST' enctype='multipart/form-data'>
+                            <input type='hidden' name='id_pokemon' value='" . $fila['id_pokemon'] . "'>
+                        
+                            <label for='nombrePokemon'>Nombre</label>
+                            <input type='text' name='nombre' value='" . $fila['nombre'] . "' class='w3-input'>
+                            
+                            <label for='imagenNueva'>Imagen del Pokémon:</label>
+                            <input type='file' name='imagenNueva'><br><br>
+                            
+                            <label for='descripcionPokemon'>Descripción</label>
+                            <textarea name='descripcion' class='w3-input'>" . $fila['descripcion'] . "</textarea>
+                            
+                            <label for='tipoPokemon'>Tipo</label>
+                            <input type='text' name='tipo' value='" . $fila['tipo'] . "' class='w3-input'>
+                            
+                            <label for='tipo2Pokemon'>Tipo 2</label>
+                            <input type='text' name='tipo_2' value='" . $fila['tipo_2'] . "' class='w3-input'>
+                            <footer class='w3-container w3-green'>
+                                    <button type='submit' class='w3-button w3-blue'>Guardar Cambios</button>
+                                    <button type='button' onclick=\"document.getElementById('modal-modificar-" . $fila['id_pokemon'] . "').style.display='none'\" class='w3-button w3-red'>Cancelar</button>
+                            </footer>
+                        </form>
                     </div>
-                </div>";
+                </div>
+            </div>";
         }
     } else {
         echo "<tr><td class='noEncontrado' colspan='5'>No se encontraron Pokémon</td></tr>";
@@ -298,5 +314,3 @@ function traerPokemonsFiltrados(mysqli $conn, $busqueda, string $path_tipos, $re
 }
 
 ?>
-
-
--- src/modificarPokemon.php
+++ src/modificarPokemon.php
@@ -1,1 +1,72 @@
 <?php
+include 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id_pokemon = $_POST['id_pokemon'];
+    $nombre = $_POST['nombre'];
+    $descripcion = $_POST['descripcion'];
+    $tipo = $_POST['tipo'];
+    $tipo_2 = $_POST['tipo_2'];
+    $ruta_absouluta = "/PokedexTp/img/pokemon/";
+
+    if (!empty($_FILES['imagenNueva']['name'])) {
+        $path_img = "../img/pokemon/";
+        $target_file = $path_img . basename($_FILES["imagenNueva"]["name"]);
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+        $check = getimagesize($_FILES["imagenNueva"]["tmp_name"]);
+        if ($check !== false) {
+            if (move_uploaded_file($_FILES["imagenNueva"]["tmp_name"], $target_file)) {
+                $ruta_imagen = $ruta_absouluta . basename($_FILES["imagenNueva"]["name"]);
+
+                $sql = "UPDATE pokemon 
+                    SET nombre = ?, descripcion = ?, tipo = ?, tipo_2 = ?, imagen = ?
+                    WHERE id_pokemon = ?";
+
+                if ($stmt = $conn->prepare($sql)) {
+                    $stmt->bind_param('sssssi', $nombre, $descripcion, $tipo, $tipo_2, $ruta_imagen, $id_pokemon);
+
+                    if ($stmt->execute()) {
+                        echo "Pokémon modificado exitosamente con nueva imagen.";
+                        header('Location: ../index.php');
+                        exit();
+                    } else {
+                        echo "Error al modificar el Pokémon.";
+                    }
+
+                    $stmt->close();
+                } else {
+                    echo "Error en la preparación de la consulta.";
+                }
+            } else {
+                echo "Error al cargar la imagen.";
+            }
+        } else {
+            echo "El archivo no es una imagen.";
+        }
+    } else {
+        $sql = "UPDATE pokemon 
+            SET nombre = ?, descripcion = ?, tipo = ?, tipo_2 = ?
+            WHERE id_pokemon = ?";
+
+        if ($stmt = $conn->prepare($sql)) {
+            $stmt->bind_param('ssssi', $nombre, $descripcion, $tipo, $tipo_2, $id_pokemon);
+
+            if ($stmt->execute()) {
+                echo "Pokémon modificado exitosamente.";
+                header('Location: ../index.php');
+                exit();
+            } else {
+                echo "Error al modificar el Pokémon.";
+            }
+
+            $stmt->close();
+        } else {
+            echo "Error en la preparación de la consulta.";
+        }
+    }
+}
+
+
+
+?>
